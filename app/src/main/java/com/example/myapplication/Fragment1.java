a897a57a36bb66a71bf419f51a9138fd6feb7f73
SanggeunParrk <55074054+SanggeunParrk@users.noreply.github.com>
2020-02-27 20:28:57 +0900

    Toast error

--- app/src/main/java/com/example/myapplication/Fragment1.java
+++ app/src/main/java/com/example/myapplication/Fragment1.java
@@ -1,6 +1,110 @@
 package com.example.myapplication;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Calendar;
+
 public class Fragment1 extends Fragment {
+    private EditText et_name, et_age, et_birthday;
+    private Button btn_send;
+    private DatePickerDialog.OnDateSetListener listener;
+    public interface OnDataPass {
+        public void onDataPass(String info);
+    }
+
+    OnDataPass dataPasser;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        dataPasser = (OnDataPass) context;
+    }
+    public void passData(String data) {
+        dataPasser.onDataPass(data);
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1,container,false);
+
+        et_birthday = rootView.findViewById(R.id.et_birthday);
+        et_name = rootView.findViewById(R.id.et_name);
+        et_age = rootView.findViewById(R.id.et_age);
+        btn_send = rootView.findViewById(R.id.btn_send);
+        final String name = et_name.getText().toString();
+        String age = et_age.getText().toString();
+        String birthday = et_birthday.getText().toString();
+        final String Info = (String) name + age + birthday;
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                passData("abc");
+                et_age.setText(null);
+                et_birthday.setText(null);
+                et_name.setText(name);
+            }
+        });
+
+        return rootView;
+    }
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        et_birthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                showDatePicker();
+            }
+        });
+    }
+
+    private void showDatePicker() {
+        DatePickerFragment date = new DatePickerFragment();
+        /**
+         * Set Up Current Date Into dialog
+         */
+        Calendar calender = Calendar.getInstance();
+        Bundle args = new Bundle();
+        args.putInt("year", calender.get(Calendar.YEAR));
+        args.putInt("month", calender.get(Calendar.MONTH));
+        args.putInt("day", calender.get(Calendar.DAY_OF_MONTH));
+        date.setArguments(args);
+        /**
+         * Set Call back to capture selected date
+         */
+        date.setCallBack(ondate);
+        date.show(getFragmentManager(), "Date Picker");
+    }
+
+    DatePickerDialog.OnDateSetListener ondate = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+
+            et_birthday.setText(String.valueOf(dayOfMonth) + "-" + String.valueOf(monthOfYear+1)
+                    + "-" + String.valueOf(year));
+        }
+    };
 }
+
+
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+implements Fragment1.OnDataPass{
+    private DatePickerDialog.OnDateSetListener callbackMethod;
+    String mInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragment1 fragment1 = new Fragment1();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
+        Toast.makeText(getApplicationContext(),mInfo,Toast.LENGTH_LONG).show();
+
     }
+    public void onDataPass(String info){
+        mInfo = info;
+    }
+
 }
